7bbf335d406cde4d87e04e96a6a38cc7934ac65b
Norbert Braun <norbertbraun@Norberts-MacBook-Pro.local>
2019-01-08 23:10:58 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -14,12 +14,26 @@ class App extends Component {
     temperatures: []
   }
 
-  temps = [];
-
   componentDidMount() {
-    //TODO: Refactor by Norbert aki egy kiraJ
-    this.state.cities.forEach((city) => {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.name}&units=metric&APPID=223f47396554c887b340484e1e0ab958`)
+    //getting the cities array from the state
+    const { cities } = this.state
+
+    //fetching cities in similar order as presented in state.cities
+    Promise.all(
+      cities.map( city => 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.name}&units=metric&APPID=223f47396554c887b340484e1e0ab958`)
+          .then(res => res.json())
+      )
+    )
+    .then(temps => {
+      //right now, temps is an array of 5 objects. (5 city), so lets populate state.temperatures with that.
+      this.setState({
+        temperatures: temps
+      })
+    })
+
+    /*this.state.cities.forEach((city) => {
+      
         .then(res => res.json())
         .then(json => {
           this.temps.push(json.main.temp);
@@ -28,7 +42,7 @@ class App extends Component {
           })
         })
         //miért nem müködött a setstate a .then-en kivül?????????????????
-    })
+    }) */
   }
 
   render() {
@@ -36,10 +50,13 @@ class App extends Component {
       <div>
         <h1>Weather app in React</h1>
         <div className="list">
-          {
+          { //ternary operation. condition ? true : false. If temperatures array is empty that means we are still fetching data
+            //so lets show a loading div. If its not empty we loop throught the array and render CityCards
+            this.state.temperatures.length < 1 ? 
+            <div> Loading ...</div> :
             this.state.cities.map((city, index) => {
               return (
-                <CityCard name={city.name} temp={this.state.temperatures[index]} key={index}></CityCard>
+                <CityCard name={city.name} temp={this.state.temperatures[index].main.temp} key={index}></CityCard>
               )
             })
           }
